8f4eae79428bd17f34d9fe8590ab43d1937c8222
Viacheslav Ostroglyad <Slait93@bk.ru>
2024-08-09 11:51:33 +0300

    for Alexandr

--- src/shared/tables/customTargetsTable.ts
+++ src/shared/tables/customTargetsTable.ts
@@ -32,38 +32,48 @@ export const CustomTargetTable = styled(DataGrid)({
 	},
 
 	//Scrollbar styles
-
-	'.MuiDataGrid-scrollbar::-webkit-scrollbar': {
+	'.MuiDataGrid-scrollbar--vertical ': {
+		width: '12px',
+	},
+	'.MuiDataGrid-scrollbarContent, & .css-1b9e9gy': {
+		width: '12px',
+	},
+	'& .MuiDataGrid-scrollbar--horizontal, & .css-1rtad1': {
+		height: '12px',
+	},
+	// '.css-1rtad1': {
+	// 	height: '12px',
+	// },
+	'& ::-webkit-scrollbar': {
 		width: '12px',
 		height: '12px',
 	},
-	'.MuiDataGrid-scrollbar::-webkit-scrollbar-track': {
+	'& ::-webkit-scrollbar-track': {
 		backgroundColor: '#06344dff',
 	},
-	'.MuiDataGrid-scrollbar::-webkit-scrollbar-thumb': {
+	'& ::-webkit-scrollbar-thumb': {
 		backgroundColor: '#0c4c5cff',
 	},
-	'.MuiDataGrid-scrollbar::-webkit-scrollbar-thumb:hover': {
+	'& ::-webkit-scrollbar-thumb:hover': {
 		backgroundColor: '#aeafaf63',
 	},
 
-	'.MuiDataGrid-scrollbar::-webkit-scrollbar-button:vertical:decrement': {
+	'& ::-webkit-scrollbar-button:vertical:decrement': {
 		backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 256'%3E%3Cpath fill='%23dfdfdfff' d='M213.66 165.66a8 8 0 0 1-11.32 0L128 91.31 53.66 165.66a8 8 0 0 1-11.32-11.32l80-80a8 8 0 0 1 11.32 0l80 80a8 8 0 0 1 0 11.32z'/%3E%3C/svg%3E")`,
 		backgroundSize: '100%',
 		backgroundRepeat: 'no-repeat',
 		backgroundPosition: 'center',
 	},
-	'.MuiDataGrid-scrollbar::-webkit-scrollbar-button:vertical:increment': {
+	'& ::-webkit-scrollbar-button:vertical:increment': {
 		backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='64' height='64' fill='%23dfdfdfff' viewBox='0 0 256 256'%3E%3Cpath d='M213.66,101.66l-80,80a8,8,0,0,1-11.32,0l-80-80A8,8,0,0,1,53.66,90.34L128,164.69l74.34-74.35a8,8,0,0,1,11.32,11.32Z'%3E%3C/path%3E%3C/svg%3E")`,
 		backgroundSize: '100%',
 		backgroundRepeat: 'no-repeat',
 		backgroundPosition: 'center',
 	},
-	'.MuiDataGrid-scrollbar::-webkit-scrollbar-button:vertical': {
+	'& ::-webkit-scrollbar-button:vertical': {
 		backgroundColor: ' #0c4c5cff',
-		ariaHidden: 'false',
 		':hover': {
-			backgroundColor: '#aeafaf63',
+			backgroundColor: '#587c7cff',
 		},
 	},
 })
